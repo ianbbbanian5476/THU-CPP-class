886b061c0512d9bab395e989d04b4c2586bba47d
ianbbbanian5476 <69791767+ianbbbanian5476@users.noreply.github.com>
2023-02-27 20:54:05 +0800

    2/27

---
+++ C++/1.cpp
@@ -0,0 +1,15 @@
+// practice1-1.cpp
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, ";
+    cout << "how are you? \n";
+    cout << "I am fine, ";
+    cout << "thank you. " << endl;
+    cout << "And you？\n";
+    cout << "Over";
+
+    return 0;
+}
