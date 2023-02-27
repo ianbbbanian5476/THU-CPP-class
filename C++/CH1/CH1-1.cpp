5df4b4a72aaa44c9d88b3179689acf68bc89d100
ianbbbanian5476 <69791767+ianbbbanian5476@users.noreply.github.com>
2023-02-27 21:10:49 +0800

    2/27

---
+++ C++/CH1/CH1-1.cpp
@@ -0,0 +1,12 @@
+// practice1-2.cpp
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int total = 0;
+    total = (17 - 9) * 60 * 60;
+    cout << "total=" << total << endl;
+
+    return 0;
+}
