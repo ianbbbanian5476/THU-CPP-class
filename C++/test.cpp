49c520c9b6b2e354c56e8b5dfda028d14dbf692b
ianbbbanian5476 <69791767+ianbbbanian5476@users.noreply.github.com>
2023-04-20 10:58:13 +0800

    4/20

--- C++/test.cpp
+++ C++/test.cpp
@@ -5,25 +5,26 @@
 
 using namespace std;
 
+vector<string> split_string(const string &str, const string &delimiter)
+{
+    vector<string> substrings;
+    size_t start = 0;
+    size_t end = str.find(delimiter);
+    while (end != string::npos)
+    {
+        substrings.push_back(str.substr(start, end - start));
+        start = end + delimiter.length();
+        end = str.find(delimiter, start);
+    }
+    substrings.push_back(str.substr(start));
+    return substrings;
+}
+
 int main()
 {
     string txt = "hello world";
-    vector<string> words;
-    stringstream ss(txt);
-    string word;
-
-    // Extract words from the stringstream
-    while (ss >> word)
-    {
-        cout << word << endl;
-        words.push_back(word);
-    }
-
-    // Print the words
-    for (string w : words)
-    {
-        cout << w << endl;
-    }
+    string delimiter = " ";
+    cout << split_string(txt, delimiter) << endl;
 
     return 0;
 }
