6302ce46a6383a971e6014f91aca877ad42d8aac
YCAIR <69791767+ianbbbanian5476@users.noreply.github.com>
2023-04-25 19:08:25 +0800

    4/25

--- C++/CH9/CH9-1.cpp
+++ C++/CH9/CH9-1.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main()
+void insertionsort(int *num, int *n)
 {
-    int num[5] = {18, 2, 20, 34, 12};
-    int n = sizeof(num) / sizeof(num[0]);
-
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i < *n - 1; i++)
     {
-        for (int j = 0; j < n - i - 1; j++)
+        for (int j = 0; j < *n - i - 1; j++)
         {
             if (num[j] > num[j + 1])
             {
@@ -18,6 +15,15 @@ int main()
             }
         }
     }
+}
+
+int main()
+{
+    int num[5] = {18, 2, 20, 34, 12};
+    int n = sizeof(num) / sizeof(num[0]);
+
+    insertionsort(num, &n);
+
     for (int z = 0; z < n; z++)
         cout << num[z] << " ";
     cout << endl;
--- C++/CH9/CH9-2.cpp
+++ C++/CH9/CH9-2.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+void add(int (*sum)[3][3], int (*num1)[3][3], int (*num2)[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            (*sum)[i][j] = (*num1)[i][j] + (*num2)[i][j];
+        }
+    }
+}
+
 int main()
 {
     int num1[3][3], num2[3][3], sum[3][3];
@@ -25,13 +36,7 @@ int main()
         }
     }
 
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            sum[i][j] = num1[i][j] + num2[i][j];
-        }
-    }
+    add(&sum, &num1, &num2);
 
     cout << "sum[3][3]" << endl;
     for (int i = 0; i < 3; i++)
