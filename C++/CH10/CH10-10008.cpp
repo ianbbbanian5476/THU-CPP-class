7741c7f5a3fea9ff6a7676d60334281abd44e9ef
ianbbbanian5476 <69791767+ianbbbanian5476@users.noreply.github.com>
2023-05-04 14:02:18 +0800

    5/4

--- C++/CH10/CH10-10008.cpp
+++ C++/CH10/CH10-10008.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <algorithm>
 #include <cctype>
+#include <vector>
 using namespace std;
 
 int main()
@@ -9,10 +10,12 @@ int main()
     map<char, int> dict;
     int round;
     cin >> round;
+    cin.ignore();
+
     for (int i = 0; i < round; i++)
     {
         string txt;
-        cin >> txt;
+        getline(cin, txt);
         for (char letter : txt)
         {
             if (isalpha(letter))
@@ -22,8 +25,23 @@ int main()
             }
         }
     }
-    for (const auto &p : dict)
+    vector<pair<char, int>> result;
+    for (auto p : dict)
+    {
+        result.push_back(p);
+    }
+    sort(result.begin(), result.end(), [](auto &left, auto &right)
+         {
+        if (left.second != right.second) {
+            return left.second > right.second;
+        } else {
+            return left.first < right.first;
+        } });
+
+    for (auto p : result)
     {
         cout << p.first << " " << p.second << endl;
     }
+
+    return 0;
 }
